611df66e2d4ce4c4f8d080bdb3050e253745bd60
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2022-12-29 21:10:39 +0500

    Try again

---
+++ src/test/java/com/company/pandaproserver/PandaProServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.pandaproserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaProServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
